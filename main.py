43dc177fb875d88cceebdafec381145a5fd21929
Muhammad Talha <>
2021-04-23 12:55:50 +0000

    ??jf

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Functional Programming
+from functools import reduce
 # A clean Functionis the one that does not affect the outsidde world 
 # Let's talk about map function
 def multiply_by2(item):
@@ -21,3 +22,12 @@ zip_list1=[1,2,3,4,5]
 zip_list2=[6,7,8,9,10]
 print(list(zip(zip_list1,zip_list2)))
 # What zip function does is it takes the firts item of the first list and takes the first item of the second list and zip them togather 
+# We can apply the zip function on more than 2 list and as many list as we want
+# Let's talk about reduce function
+# To use reduce we have to import it ,it does not come with python as built in we have to import it using the functool package you can Say 
+def accumulator(acc,item):
+  print(acc,item)
+  return acc+item
+my_list=[1,2,3]
+print(reduce(accumulator,my_list,1))
+print(my_list)
